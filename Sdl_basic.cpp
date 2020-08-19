441372a9ec9f0ee1b681454318130a0076192d2d
byeong cheol lim <byeongcheollim@byeongui-MacBookPro.local>
2020-08-20 01:25:46 +0900

    sdl activated

---
+++ Sdl_basic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<SDL2/SDL.h>
+using namespace std;
+int main() {
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        cout << "SDL init failed" << endl;
+        return 1;
+    }
+    cout << "SDL init succeeded" << endl;
+    SDL_Quit();
+    return 0;
+}
+
